5926e924acf66add1dc243019b8c6080ac6d62f7
po3nx <ipo3nx@gmail.com>
2024-03-28 10:43:54 +0800

    Update HlJS Style

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import '@fortawesome/fontawesome-free/css/all.css';
-import 'highlight.js/styles/default.min.css';
+import 'highlight.js/styles/github-dark.min.css';
 import './assets/styles/tailwind.css'
 
 createApp(App).mount('#app')
